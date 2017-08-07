f06f4f71a570cf4aa5cc9af915b1a2f7e0675c7c
Frank Stokes <frank@wearereasonablepeople.com>
2017-08-07 09:25:56 +0200

    Improving naming

--- src/evaluate/evaluate-expr/language-core/index.js
+++ src/evaluate/evaluate-expr/language-core/index.js
@@ -2,13 +2,13 @@ const extensions = require('../../../extensions');
 
 module.exports = Object.assign(
   {
-    if: require('./if-statement'),
+    if: require('./if'),
     let: require('./let-assign'),
     function: require('./create-function'),
     lambda: require('./lambda'),
     list: require('./list'),
-    map: require('./map-list'),
-    filter: require('./filter-list'),
+    map: require('./map'),
+    filter: require('./filter'),
     call: require('./call-by-reference'),
     apply: require('./apply'),
     mutate: require('./mutate'),
