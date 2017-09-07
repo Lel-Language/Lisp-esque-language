a0ea707182567c196503698237fe3223d6b4bbb7
Omer Saatcioglu <osaatcioglu@gmail.com>
2017-09-07 10:11:28 +0200

    Improvement: Define recursive function findBasePath explicitly  (#37)
    * Improvement: It doesn’t look like findBasePath is defined. But it works even though I sometimes get a runtime error for that. Yet, I defined the function explicitly.
    
    * Use non private naming convention

--- src/evaluate/evaluate-expr/find-basepath.js
+++ src/evaluate/evaluate-expr/find-basepath.js
@@ -1,4 +1,6 @@
-module.exports = (scope) => {
+const findBasepath = (scope) => {
   if (scope.upperScope) return findBasepath(scope.upperScope);
   return scope.basepath;
-};
+}
+
+module.exports = findBasePath;
